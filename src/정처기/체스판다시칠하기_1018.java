7493bd2cf9a10cc61743235bc44bfe17fbe0e94c
gunny6026 <gunny6026@naver.com>
2021-06-29 03:27:30 -0400

    TIL 데이터 타입

---
+++ src/정처기/체스판다시칠하기_1018.java
@@ -0,0 +1,12 @@
+package 정처기;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 체스판다시칠하기_1018 {
+    public static void main(String[] args) {
+        new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+}
